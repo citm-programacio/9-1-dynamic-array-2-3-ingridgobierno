3bcbf711c882a9344535e25de4eb5278fe0fdc1f
Ingrid <ingrid.lara@estudiantat.upc.edu>
2024-11-13 13:20:36 +0100

    Update dynamic-array-2-3.cpp
    part 1 feta

--- dynamic-array-2-3.cpp
+++ dynamic-array-2-3.cpp
@@ -1,19 +1,59 @@
-// dynamic-array-2-3.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-
 #include <iostream>
+#include <string>
+using namespace std;
+
+//int* ordena(int *number, int length) {
+//	int* copia = new int[length];
+//
+//	for (int i = 0; i < length - 1; i++) {
+//		for (int j = 0; j < length - i - 1; j++) {
+//			if (number[j] > number[j + 1]) {
+//				int copia = number[j];
+//				number[j] = number[j + 1];
+//				number[j + 1] = copia;
+//				cout << copia;
+//			}
+//		}
+//	}
+//	delete[] number;
+//	number = copia;
+//	return copia;
+//	
+//
+//}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int num;
+	int len = 0;
+	int* number = new int[len];
+	for (int i = 0; i <= len; i++){
+		cout << "\nDigues un numero: ";
+		cin >> num;
+		len++;
+
+		int* newnums = new int[len];
+		for (int i = 0; i <= len-1; i++) {
+			newnums[i] = number[i];
+		}
+		newnums[len-1] = num;
+		delete[] number;
+		number = newnums;
+
+		cout << "[";
+		for (int j = 0; j < len; j++) {
+			cout << number[j] << ",";
+		}
+		cout << "]" << endl;
+
+
+		if (len == 10) {
+			break;
+		}
+	}
+	//ordena(number, len);
+
+	
+
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
